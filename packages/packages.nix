5f3adbf27f29b0759cd0603fad5474bbbb94606e
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-10-18 21:59:25 +0200

    Remove packages

--- packages/packages.nix
+++
@@ -1,9 +0,0 @@
-{
-  perSystem = {pkgs, ...}: {
-    packages = {
-      cargo-contract = pkgs.callPackage ./cargo-contract/_package.nix {};
-      rustc-with-src = pkgs.callPackage ./rustc-with-src/_package.nix {};
-      substrate-contracts-node = pkgs.callPackage ./substrate-contracts-node/_package.nix {};
-    };
-  };
-}
