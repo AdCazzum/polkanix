d044aabfeddce8c55cabebf0bfe450067cdeb4c6
Andrea Ciceri <andrea.ciceri@autistici.org>
2025-10-18 23:16:05 +0200

    Remove virtualbox exporters

--- nixos/exporters.nix
+++ nixos/exporters.nix
@@ -12,32 +12,27 @@
     }
   ];
 
-  # Generate image with optional extra modules
   mkImage = format: extraModules: inputs.nixos-generators.nixosGenerate {
     system = "x86_64-linux";
     inherit format;
     modules = baseModules ++ extraModules;
   };
 
-  # Cloud images need to disable sudo.execWheelOnly
   cloudImageModule = {lib, ...}: {
     security.sudo.execWheelOnly = lib.mkForce false;
   };
 
-  # Some formats configure their own bootloader
   noBootloaderModule = {lib, ...}: {
     boot.loader.systemd-boot.enable = lib.mkForce false;
     boot.loader.efi.canTouchEfiVariables = lib.mkForce false;
   };
 
-  # VirtualBox needs more resources for building
   virtualboxModule = {
     virtualbox.params.diskSize = 16384; # 16GB instead of default 8GB
     virtualbox.params.memorySize = 2048; # 2GB RAM for the final VM
     virtualbox.memorySize = 4096; # 4GB RAM for the build VM
   };
 
-  # LXC containers need special network configuration
   lxcModule = {lib, ...}: {
     networking.useHostResolvConf = lib.mkForce false;
     services.resolved.enable = lib.mkForce true;
@@ -45,10 +40,8 @@
 in {
   flake = {
     packages.x86_64-linux = {
-      # QEMU/KVM
       qcow2 = mkImage "qcow" [];
 
-      # Cloud providers
       ami = mkImage "amazon" [
         inputs.srvos.nixosModules.hardware-amazon
         cloudImageModule
@@ -75,23 +68,16 @@ in {
         noBootloaderModule
       ];
 
-      # Oracle Cloud Infrastructure - using raw-efi format
       oracle = mkImage "raw-efi" [
         cloudImageModule
       ];
 
-      # Virtualization
       vmware = mkImage "vmware" [];
-      virtualbox = mkImage "virtualbox" [
-        noBootloaderModule
-        virtualboxModule
-      ];
       proxmox = mkImage "proxmox" [
         noBootloaderModule
       ];
       hyperv = mkImage "hyperv" [];
 
-      # Container
       lxc = mkImage "lxc" [
         lxcModule
         noBootloaderModule
